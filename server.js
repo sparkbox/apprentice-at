36ba6a810474a4d4dc25b5dd75ba74005eb7493b
Nathan Rambeck <nathan@heysparkbox.com>
2017-12-19 15:44:49 -0500

    feat: redirect all traffic to https

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 var express = require('express');
+var sslRedirect = require('heroku-ssl-redirect');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+// enable ssl redirect
+app.use(sslRedirect());
 app.use(express.static('dist'));
 
 var server = app.listen(app.get('port'), () => {
