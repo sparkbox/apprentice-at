73c91173b542939cb9537dc1ea4ac586e5058f25
saashapina <saasha@heysparkbox.com>
2022-02-17 16:24:49 -0500

    testing 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -7,16 +7,19 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addHandlebarsHelper('forEach', helpers.forEach)
   eleventyConfig.addDataExtension('yml', (contents) => yaml.load(contents))
   return {
+    templateFormats: ['hbs', 'md', '11ty.js'],
+    htmlTemplateEngine: 'hbs',
+    passthroughFileCopy: true,
     // configuration object for directories
     dir: {
       // Eleventy will look here for files to process
-      input: 'source/templates/pages',
+      input: 'source/templates/pages/about.hbs',
       // the built files will be placed here
-      output: 'dist',
+      output: '_site',
       // tells Eleventy where to look for layouts/partials relative to input
       includes: '../partials',
       layouts: '../layouts',
-      data: '../../data',
+      // data: '../data',
     },
   };
 };
