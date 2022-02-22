6daaa295c66a890689af382ca58e62d155e97ccb
saashapina <saasha@heysparkbox.com>
2022-02-22 16:26:01 -0500

    Refactor: Update form validation

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,38 +1,63 @@
-var fieldValidation, validation;
-
-fieldValidation = function(checkThis) {
-  var $checkThis, isValid;
-  isValid = true;
-  $checkThis = $(checkThis);
-  if ($.trim($checkThis.val()).length) {
-    $checkThis.removeClass('invalid');
-  } else {
-    isValid = false;
-    $checkThis.addClass('invalid');
-  }
-  return isValid;
-};
-
-validation = function() {
-  $('form').submit(function() {
-    var $inputs, isValid;
-    isValid = true;
-    $inputs = $(this).find('[required]');
-    $inputs.each(function() {
-      var $input;
-      $input = $(this);
-      if (!fieldValidation($input)) {
-        isValid = false;
+const validation = function() {
+  $('form').validate({
+    rules: {
+      name: "required",
+      url: {
+        required: true,
+        url: true
+      },
+      category: "required",
+      duration: {
+        required: true,
+        min: 1,
+        number: true
+      },
+      summary: "required",
+      location: "required",
+      learn_more: {
+        required: true,
+        url: true
+      },
+     apprentice: {
+        required: true,
+        email: true
+      },
+      email: {
+        required: true,
+        email: true
       }
-      return true;
-    });
-    if (!isValid) {
-      return false;
+    },
+    messages: {
+      compensation: "Your apprenticeship must be paid",
+      name: "Please enter your company name",
+      url: {
+        required: "Please enter your company website",
+        url: "Please enter a valid URL"
+      },
+      category: "Please select a category",
+      duration: {
+        required: "Please enter a duration",
+        min: "Please enter a number greater than 0",
+        number: "Please enter a number"
+      },
+      summary: "Please enter a summary of the apprenticeship",
+      location: "Please enter a location",
+      learn_more :{
+        required: "Please enter a link to the application",
+        url: "Please enter a valid URL"
+      },
+      apprentice: {
+        required: "Please enter a past apprentice email",
+        email: "Plesae enter a valid email address"
+      },
+      email: {
+        required: "Please enter an email address",
+        email: "Please enter a valid email address"
+      }
+
     }
-  });
-  $('form *').focusout(function() {
-    return fieldValidation(this);
-  });
+  })
+
   return $('input[type="radio"]').on('click', function(e) {
     if ($('#compensation_no').is(':checked')) {
       $('#compensation_no').addClass('invalid');
