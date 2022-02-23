8f14fcbd5087891a9969c36bb8cd834744eaf906
Angus Chang <angus@heysparkbox.com>
2022-02-23 14:47:33 -0500

    refactor: remove grunt tasks, replace watch task
    - Remove all unused grunt tasks
    - Install 'watch' npm package
    - created scripts to watch for js and hbs file changes
    - deleted engine property in package.json, in favor of .nvmrc file
    
    APPAT-45

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,20 +5,6 @@ module.exports = function(grunt) {
   //#### Tasks ######
   grunt.loadTasks("grunt");
 
-  grunt.registerTask("build", []);
-
-  // CircleCI Continuous Deployment Task
-  grunt.registerTask("ci", "build");
-
-  // Build for production
-  grunt.registerTask("dist", "build");
-
   // Clean Task
   grunt.registerTask("everything-clean", ["clean:all", "copy:main"]);
-
-  // Build Task
-  grunt.registerTask("dev", ["watch"]);
-
-  // Default task
-  return grunt.registerTask("default", "dev");
 };
